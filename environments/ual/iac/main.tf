b120e10e483a7596a995b2d6e11283a14273f6b7
Andriy Kandzyuba <andriy.kandzyuba@dynatrace.com>
2026-03-13 15:53:01 -0500

    [ESA-5] Remove Gen2 dashboards

--- environments/ual/iac/main.tf
+++ environments/ual/iac/main.tf
@@ -1,9 +1,4 @@
 # environments/dev/main.tf
-module "dynatrace_json_dashboard" {
-  # Path to your local module folder
-  source = "./modules/dynatrace_json_dashboard"
-}
-
 module "dynatrace_document" {
   # Path to your local module folder
   source = "./modules/dynatrace_document"
