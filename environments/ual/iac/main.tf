9aae1c5c70f5c2df744b5ebe1a60c3dc5a406020
Andriy Kandzyuba <andriy.kandzyuba@dynatrace.com>
2026-03-13 14:09:19 -0500

    [ESA-5] Add Gen3 document provisioning

--- environments/ual/iac/main.tf
+++ environments/ual/iac/main.tf
@@ -1,5 +1,10 @@
 # environments/dev/main.tf
-module "dynatrace_api_token" {
+module "dynatrace_json_dashboard" {
   # Path to your local module folder
   source = "./modules/dynatrace_json_dashboard"
+}
+
+module "dynatrace_document" {
+  # Path to your local module folder
+  source = "./modules/dynatrace_document"
 }
