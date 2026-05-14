c55234d96b421c92d37d622c143dee602019e531
Andriy Kandzyuba <andriy.kandzyuba@dynatrace.com>
2026-05-14 17:15:10 -0500

    [ESA-16] - Delete all environment specific configurations. Generate them in workflows

--- environments/ual/iac/main.tf
+++
@@ -1,9 +0,0 @@
-module "dynatrace_document" {
-  # Path to your local module folder
-  source = "./modules/dynatrace_document"
-}
-
-module "dynatrace_alerting" {
-  # Path to your local module folder
-  source = "./modules/dynatrace_alerting"
-}
