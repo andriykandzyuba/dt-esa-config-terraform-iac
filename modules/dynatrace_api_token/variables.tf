ec624f46043c6af2e51c0154299b86ba3d1143ee
Andriy Kandzyuba <andriy.kandzyuba@dynatrace.com>
2026-06-11 15:46:55 -0500

    -Fix the ttl format for a generated token

--- modules/dynatrace_api_token/variables.tf
+++ modules/dynatrace_api_token/variables.tf
@@ -6,7 +6,7 @@ variable "dt_esa_api_token_name" {
 variable "dt_esa_api_token_duration" {
     description = "Dynatrace API Token lifespan in days. The default value is set to 1 hour."
     type = string
-    default = "3d"
+    default = "72h"
 }
 
 variable "dt_esa_api_token_scope" {
